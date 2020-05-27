1f2d54e44b5de521137da8d5d404f3adc2953bf1
17jmalek98 <17jmalek98@gmail.com>
2020-05-27 15:25:04 -0700

    added bootstrap styling and page components

---
+++ tracker/src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class editExercise extends Component {
+    render() {
+        return(
+            <div>
+                <p>You are on the Edit Exercise component!</p>
+            </div>
+        )
+    }
+}
--- tracker/src/components/navbar.component.js
+++ tracker/src/components/navbar.component.js
@@ -7,7 +7,7 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+        <Link to="/" className="navbar-brand">Trackfit</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
