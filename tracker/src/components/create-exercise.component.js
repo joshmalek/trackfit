c03b93f31fbac005f0ecf1239e8694e11d4c44c8
17jmalek98 <17jmalek98@gmail.com>
2020-05-30 23:54:00 -0700

    added DatePicker package and imports

--- tracker/src/components/create-exercise.component.js
+++ tracker/src/components/create-exercise.component.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
 
 export default class createExercise extends Component {
     constructor(props){
