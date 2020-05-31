1026fa6e6630db8dc841564cdb92234a892d2285
17jmalek98 <17jmalek98@gmail.com>
2020-05-31 00:58:08 -0700

    start implement of exercise list for homepage

--- tracker/backend/routes/exercises.js
+++ tracker/backend/routes/exercises.js
@@ -3,13 +3,13 @@ let Exercise = require('../models/exercise.model');
 
 //endpoint to handle incoming http GET request on the /users path
 router.route('/').get((req, res) => {
-    //mongoose method gives a list of all the users from the mongodb database in json 
+    //mongoose method gives a list of all the exercises from the mongo database in json 
     Exercise.find()
         .then(exercises => res.json(exercises))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-//handles incoming http POST requests to add new users
+//handles incoming http POST requests to add new exercises
 router.route('/add').post((req, res) =>{
     const username = req.body.username;
     const description = req.body.description;
--- tracker/src/components/exercises-list.component.js
+++ tracker/src/components/exercises-list.component.js
@@ -1,6 +1,44 @@
 import React, {Component} from 'react';
-
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 export default class exerciseList extends Component {
+    constructor(props){
+        //need to always call super when defining the constructor of a subclass
+        super(props);
+        //asserts that when a function calls this, it refers to the right this
+        this.deleteExercise = this.deleteExercise.bind(this);
+        //a state is something that can be updated without page refresh
+        this.state = {
+            exercises: []
+        }
+    }
+
+    componentDidMount(){
+        //this code retrieves all of the exercises in the database when the page loads,
+        //and displays them in the homepage in a list
+        axios.get('http://localhost:5000/exercises/')
+            .then(response => {
+                this.setState({
+                    exercises: response.data
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+    
+    deleteExercise(id){
+        axios.delete('http://localhost:5000/exercises/'+id)
+            .then(res => console.log(res.data));
+        //whenever state is updated, page is refreshed so instead of doing another get call
+        //to retrieve the new exercises list, we filter out the exercise ID we just deleted.
+        //(efficiency maybe? saves hitting the server twice)
+
+        this.setState({
+            exercises: this.state.exercises.filter(el => el._id !== id)
+        })
+    }   
+
     render() {
         return(
             <div>
